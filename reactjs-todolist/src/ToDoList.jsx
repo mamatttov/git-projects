4a66c7628e891e52dd6e8df8fda4c424555158a9
Kairatbek Mamatov <161651285+mamatttov@users.noreply.github.com>
2026-01-22 14:54:39 +0600

    refactor: update ToDoList component to accept props for todos

--- reactjs-todolist/src/ToDoList.jsx
+++ reactjs-todolist/src/ToDoList.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ToDoCard from "./ToDoCard";
 
-export default function ToDoList() {
+export default function ToDoList(props) {
+  const { todos } = props;
   return (
     <ul className="main">
       {todos.map((todo, todoIndex) => (
